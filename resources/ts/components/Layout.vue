8b62848efb709ae798d789b41b1b4013211b2ecd
broccoli <zeratukin@gmail.com>
2023-01-16 20:51:20 +1000

    edit

--- resources/ts/components/Layout.vue
+++ resources/ts/components/Layout.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts">
-
-</script>
 
 <template>
-    
+
+
+
 </template>
+
+<script lang="ts">
+
+import {defineComponent} from 'vue'
+import { storeKey } from 'vuex'
+import store from '../store/store'
+
+export default defineComponent({
+	name: 'lauout-rum',
+
+	data() {
+		return {
+
+		}
+	},
+
+	mounted() {
+		// console.log(this.$store.state.csrf)
+	}
+})
+
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,8 @@
 @extends('layout.index')
 
 @section('title', 'Образовательная платформа')
+
+
+@section('main')
+	<lauout-rum></lauout-rum>
+@endsection
